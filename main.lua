1b47cc8125c77a5e0e91ab79bc8939e71339bb6e
Salinder Sidhu <salinder.sid@gmail.com>
2018-08-25 19:00:33 -0400

    Implemented dictionary support and exclusion of specific word fragments

--- main.lua
+++ main.lua
@@ -1,17 +1,19 @@
 local PREFIX = "[Murloc] "
 local ADDON_VERSION = GetAddOnMetadata("Mgrler", "Version")
 
-local function MgrlerAddonCommands(msg, editbox)
-    if msg ~= "" then
+local function MgrlerAddonCommands(txt, editbox)
+    if txt ~= "" then
+        -- Obtain the ID of the chat and other player if applicable
         local chatID = editbox:GetAttribute("chatType")
         local whoID = editbox:GetAttribute("tellTarget")
 
         if chatID == "WHISPER" then
-            SendChatMessage(PREFIX..Nerglish.Translate(msg), chatID, nil, whoID)
+            SendChatMessage(PREFIX..Nerglish.Translate(txt), chatID, nil, whoID)
         else
-            SendChatMessage(PREFIX..Nerglish.Translate(msg), chatID, nil)
+            SendChatMessage(PREFIX..Nerglish.Translate(txt), chatID, nil)
         end
     else
+        -- Display addon information if no txt was found
         print("|cFFFFFF00Welcome to Mgrler the Nerglish chat addon.|r")
         print("|cFFFFFF00Version: "..ADDON_VERSION.."|r")
         print("|cFFFFFF00Use: /ngl <some text here>.|r")
--- modules/nerglish.lua
+++ modules/nerglish.lua
@@ -1,8 +1,13 @@
-local _ = ...
+Nerglish = {}
 
-Nerglish = CreateFrame("Frame")
+-- Auxiliary function that returns a set constructed from a list.
+function Set(list)
+    local set = {}
+    for _, l in ipairs(list) do set[l] = true end
+    return set
+end
 
--- known words from https://wow.gamepedia.com/Nerglish
+-- Known words from https://wow.gamepedia.com/Nerglish
 local DICTIONARY = {
     aunt = "mmmrrggllm",
     uncle = "mmmrrgglm",
@@ -28,6 +33,7 @@ local DICTIONARY = {
     clan = "klun"
 }
 
+-- Maps English characters to Nerglish characters.
 local CHAR_MAP = {
     a = "mr",
     b = "gl",
@@ -57,20 +63,64 @@ local CHAR_MAP = {
     z = "lr"
 }
 
-local function firstToUpper(str)
-    return str:gsub("^%l", string.upper)
-end
+-- Strings to exclude from translation, usually emoji characters.
+local EXCLUDE = Set{
+    ":o",
+    ":O",
+    ";o",
+    ";O",
+    ";P",
+    ";p",
+    ":P",
+    ":p",
+    ":S",
+    ":s",
+    ":x",
+    ":X",
+    ":D",
+    ";D",
+    "D:",
+    ":B"
+}
 
 function Nerglish.Translate(str)
-    local res = ""
-
-    for c in str:gmatch(".") do
-        local tok = CHAR_MAP[c:lower()] or c
-        if c == c:upper() then
-            tok = firstToUpper(tok)
+    local result = ""
+    -- Iterate through all word fragments, split by whitespace
+    for word in str:gmatch("%S+") do
+        --[[
+            Append excluded word fragment directly to the result without
+            translation.
+        ]]
+        if EXCLUDE[word] then
+            result = result..word.." "
+        else
+            local lWord = word:lower()
+            local uWord = word:upper()
+            --[[
+                Attempt to substitute a matching word from the DICTIONARY. 
+                If no matching word was found then the word fragment remains
+                unchanged.
+            ]]
+            local tWord = lWord:gsub(lWord:gsub("%W+", ""), DICTIONARY)
+            --[[
+                Apply capitalization to the whole word or first character
+                depending on the original word fragment.
+            ]]
+            if lWord ~= tWord then
+                if word == uWord then
+                    tWord = tWord:upper()
+                elseif word ~= lWord then
+                    tWord = tWord:gsub("^%l", string.upper)
+                end
+                result = result..tWord.." "
+            else
+                -- Transcribe unknown words using the CHAR_MAP.
+                for c in string.gmatch(word.." ", ".") do
+                    local tC = CHAR_MAP[c:lower()] or c
+                    result = result..(c == c:upper() and tC:upper() or tC)
+                end
+            end
         end
-        res = res..tok
     end
-
-    return res
+    return result
 end
