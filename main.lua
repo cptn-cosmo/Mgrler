cdf9578592cd23d01c11d8641d708d38b027596c
cptn-cosmo <cptncosmo@gmail.com>
2025-04-03 12:49:40 +0200

    make it work in 11.1

--- main.lua
+++ main.lua
@@ -1,5 +1,9 @@
+print("Main.lua loaded!")
+
+local addonName = ...
+
 local PREFIX = "[Murloc] "
-local ADDON_VERSION = GetAddOnMetadata("Mgrler", "Version")
+local ADDON_VERSION = "0.1"
 
 local function MgrlerAddonCommands(txt, editbox)
     if txt ~= "" then
@@ -22,3 +26,17 @@ end
 
 SLASH_NGL1 = '/ngl'
 SlashCmdList["NGL"] = MgrlerAddonCommands
+
+local function OnEvent(self, event, arg1)
+    if event == "ADDON_LOADED" and arg1 == addonName then
+        print(addonName .. " loaded successfully!")
+        -- Example: Fetch metadata safely
+        local version = GetAddOnMetadata(addonName, "Version")
+        print("Version: " .. (version or "unknown"))
+    end
+end
+
+-- Create a frame to listen for events
+local frame = CreateFrame("Frame")
+frame:RegisterEvent("ADDON_LOADED")
+frame:SetScript("OnEvent", OnEvent)
